772c0795d57cf7bac2980d8ba6f886753e4ba903
Lucas Teles <lucasouzateles@gmail.com>
2024-11-04 16:04:31 -0300

    adicionando tests

--- src/test/java/io/github/lucasteles/libraryapi/repository/AuthorRepositoryTest.java
+++ src/test/java/io/github/lucasteles/libraryapi/repository/AuthorRepositoryTest.java
@@ -1,4 +1,68 @@
 package io.github.lucasteles.libraryapi.repository;
 
+import io.github.lucasteles.libraryapi.model.Author;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@SpringBootTest
 public class AuthorRepositoryTest {
+    @Autowired
+    AuthorRepository repository;
+
+    @Test
+    void saveTest() {
+        Author author = new Author();
+        author.setName("Maria");
+        author.setNationality("Brasileira");
+        author.setDateOfBirth(
+                LocalDate.of(1999, 8, 22)
+        );
+
+        var savedAuthor = repository.save(author);
+        System.out.println("Autor salvo: " + savedAuthor);
+
+    }
+
+    @Test
+    void updateTest() {
+        var id = UUID.fromString("362cfa38-2e7e-4158-9b92-372a5418b420");
+        Optional<Author> possibleAuthor = repository.findById(id);
+        if (possibleAuthor.isPresent()) {
+            Author foundAuthor = possibleAuthor.get();
+            System.out.println("Dados do Autor");
+            System.out.println(foundAuthor);
+
+            foundAuthor.setNationality("Inglesa");
+            repository.save(foundAuthor);
+        }
+    }
+
+    @Test
+    void listAuthorsTest() {
+        List<Author> listAuthors = repository.findAll();
+        listAuthors.forEach(System.out::println);
+    }
+
+    @Test
+    void countTest() {
+        System.out.println("Contagem de autores: " + repository.count());
+    }
+
+    @Test
+    void deleteByIdTest() {
+        var id = UUID.fromString("362cfa38-2e7e-4158-9b92-372a5418b420");
+        repository.deleteById(id);
+    }
+
+    @Test
+    void deleteAllTest() {
+        repository.deleteAll();
+        System.out.println("dados apagados");
+    }
 }
---
+++ src/test/java/io/github/lucasteles/libraryapi/repository/BookRepositoryTest.java
@@ -0,0 +1,42 @@
+package io.github.lucasteles.libraryapi.repository;
+
+
+import io.github.lucasteles.libraryapi.model.Author;
+import io.github.lucasteles.libraryapi.model.Book;
+import io.github.lucasteles.libraryapi.model.BookGender;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@SpringBootTest
+class BookRepositoryTest {
+    @Autowired
+    BookRepository repository;
+
+    @Autowired
+    AuthorRepository authorRepository;
+
+    @Test
+    void saveTest(){
+        Book book = new Book();
+        book.setIsbn("123456-78910");
+        book.setPrice(BigDecimal.valueOf(100));
+        book.setGender(BookGender.FANTASIA);
+        book.setTitle("Fantasias da programação");
+        book.setPublicationDate(LocalDate.of(2000, 11, 11));
+
+        Author author = authorRepository
+                .findById(UUID.fromString("48c51fa5-e333-40ae-b538-d29dfb13f8b1"))
+                .orElse(null);
+
+        book.setIdAuthor(author);
+
+        repository.save(book);
+    }
+
+
+}

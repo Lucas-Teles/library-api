371f00ffbfa08f26c498fbdf2ac847477cc68ff0
Lucas Teles <lucasouzateles@gmail.com>
2024-11-04 13:55:14 -0300

    configurando inicialização db

--- src/main/java/io/github/lucasteles/libraryapi/config/DatabaseConfiguration.java
+++ src/main/java/io/github/lucasteles/libraryapi/config/DatabaseConfiguration.java
@@ -1,2 +1,27 @@
-package io.github.lucasteles.libraryapi.config;public class DatabaseConfiguration {
+package io.github.lucasteles.libraryapi.config;
+
+import io.github.lucasteles.libraryapi.repository.AuthorRepository;
+import io.github.lucasteles.libraryapi.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfiguration implements CommandLineRunner {
+    public static final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public DatabaseConfiguration(AuthorRepository authorRepository, BookRepository bookRepository){
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception{
+        log.info("BORA LUCÃO");
+        log.info("O banco conectou :D");
+    }
+
 }
